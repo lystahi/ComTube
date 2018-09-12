3bae223dff2fc2017cb296cef58b14627f990945
Taiga Hirano <taiga.hirano@gmail.com>
2018-09-12 21:37:43 +0900

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit,
+  before_action :user_signed_in?, only: [:index, :edit,
                                         :update, :destroy,
                                         :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
@@ -10,12 +10,74 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
 
-  def new
+  # GET /users/:id
+  def show
+    @user       = User.find(params[:id])
+    @videoposts = @user.videoposts.paginate(page: params[:page])
+    # => app/views/users/show.html.erb
+    # debugger
   end
 
-  def Show
-    @current_user = current_user
-    @videoposts = @current_user.videoposts.paginate(page: params[:page])
+  # GET /users/new
+  def new
+    @user = User.new
+    # => form_for @user
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save # => Validation
+      # Sucess
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
+    else
+      # Failure
+      render 'new'
+    end
+  end
+
+  # GET /users/:id/edit
+  # params[:id] => :id
+  def edit
+    @user = User.find(params[:id])
+    # => app/views/users/edit.html.erb
+  end
+
+  #PATCH /users/:id
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Success
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      # Failure
+      # => @user.errors.full_messages()
+      render 'edit'
+    end
+  end
+
+  # DELETE /users/:id
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :videoposts, only:[:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
