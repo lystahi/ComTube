f99c3013f2d90fc8c3a20b606740438f262c5601
Taiga Hirano <taiga.hirano@gmail.com>
2018-09-19 03:28:31 +0900

    Fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_14_011802) do
+ActiveRecord::Schema.define(version: 2018_09_18_182515) do
 
   create_table "follows", force: :cascade do |t|
     t.string "followable_type", null: false
@@ -63,10 +63,10 @@ ActiveRecord::Schema.define(version: 2018_09_14_011802) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "provider"
-    t.string "uid"
     t.string "username"
     t.string "name"
+    t.string "provider"
+    t.string "uid"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
