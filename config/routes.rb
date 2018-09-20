446319a28cc1ea0047a26e918b54ca500dc6ae43
Taiga Hirano <taiga.hirano@gmail.com>
2018-09-20 21:33:56 +0900

    Add privacy policy

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+  get  '/privacy_policy', to: 'static_pages#privacy_policy'
   get  '/signup',  to: 'users#new'
   get  '/search',  to: 'static_pages#search'
   resources :users do
